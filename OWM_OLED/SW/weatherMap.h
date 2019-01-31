4f9168256517f05f010cc97ba12e9582a5a3f678
Thibault MARECHAL <tmarechal.projects@gmail.com>
2019-01-31 21:39:51 +0100

    Refactoring the root directory splitting Hardware and Software, adding hw parts(BOM and schematics).

---
+++ OWM_OLED/SW/weatherMap.h
@@ -0,0 +1,14 @@
+
+#ifndef WEATHERMAP_H_
+#define WEATHERMAP_H_
+
+const char* host = "api.openweathermap.org";
+const int   httpsPort = 443;
+const char* openWeatherMapUrl = "/data/2.5/weather";
+const char* openWeatherMapId = "3081368"; // Wroclaw, PL
+const char* openWeatherMapQ = "Wroclaw"; //
+const char* openWeatherMapUnits = "metric"; // Celsius
+const char* openWeatherKey = "a2ab272b15cbf3105ea81634d405bf2f";
+String param = String(openWeatherMapUrl) + "?q=" + openWeatherMapQ + "&units=" + openWeatherMapUnits + "&APPID=" + openWeatherKey;
+
+#endif
---
+++ OWM_OLED/SW/wifiSettings.h
@@ -0,0 +1,8 @@
+
+#ifndef WIFISETTINGS_H_
+#define WIFISETTINGS_H_
+
+char* wifi_ssid = "OnePlus6";
+char* wifi_password = "cd5ed0cec136";
+
+#endif
